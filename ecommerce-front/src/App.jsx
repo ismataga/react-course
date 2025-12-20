2555b312124b083f9ea2bfbf9a73fc92f49bc6e0
Ismat Aghamirzayev <ismataga1@gmail.com>
2025-12-20 12:17:44 +0400

    lift vcard up shared beetwen

--- ecommerce-front/src/App.jsx
+++ ecommerce-front/src/App.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/CheckoutPage";
@@ -5,21 +6,21 @@ import { OrdersPage } from "./pages/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
 
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
 
-  // GIT WORKS TEST
-  // FORCE GIT CHANGE 123
-
+  useEffect;
+  axios.get("/api/cart-items").then((response) => {
+    setCart(response.data);
+  }, []);
 
   return (
     <Routes>
-      <Route index element={<HomePage />} />
+      <Route index element={<HomePage cart={cart} />} />
 
-
-
-
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
     </Routes>
