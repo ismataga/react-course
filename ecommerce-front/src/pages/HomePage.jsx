5202d1211758575e14d2119c3f99ff462670972f
Ismat Aghamirzayev <ismataga1@gmail.com>
2025-12-20 12:09:51 +0400

    add sercver proxy config /api and /images

--- ecommerce-front/src/pages/HomePage.jsx
+++ ecommerce-front/src/pages/HomePage.jsx
@@ -5,15 +5,15 @@ import "./HomePage.css";
 
 export function HomePage() {
   const [products, setProducts] = useState([]);
-  const [cart setCart] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/products").then((response) => {
+    axios.get("/api/products").then((response) => {
       setProducts(response.data);
     });
 
-    axios.get("http://localhost:3000/api/cart-items").then((response) => {
-     setCart(response.data);
+    axios.get("/api/cart-items").then((response) => {
+      setCart(response.data);
     });
   }, []);
 
