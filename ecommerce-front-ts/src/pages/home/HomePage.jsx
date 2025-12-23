4019f094337038b30095e7f6dcc2d00465d8e93a
Ismat Aghamirzayev <ismataga1@gmail.com>
2025-12-23 07:01:19 +0400

    covert project to ts

---
+++ ecommerce-front-ts/src/pages/home/HomePage.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Header } from "../../components/Header";
+import "./HomePage.css";
+import { ProductGrid } from "./ProductGrid";
+
+export function HomePage({ cart, loadCart }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getHomeData = async () => {
+      const response = await axios.get("/api/products");
+      setProducts(response.data);
+    };
+
+    getHomeData();
+  }, []);
+
+  return (
+    <>
+      <title>Ecommerce Project</title>
+
+      <Header cart={cart} />
+
+      <div className="home-page">
+        <ProductGrid products={products} loadCart={loadCart} />
+      </div>
+    </>
+  );
+}
